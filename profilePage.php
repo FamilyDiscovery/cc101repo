f007708bbac877696127d057fcb69c847068dea2
Binny <binnylewis@gmail.com>
2016-02-12 12:03:07 -0500

    Use table display for family tree - update12 - ajax2

--- profilePage.php
+++ profilePage.php
@@ -427,7 +427,7 @@
             //-----------------------------------------------------------------------
             $.ajax({
                 url: 'addFam.php',                  //the script to call to get data
-                data: "",                        //you can insert url argumnets here to pass to api.php
+                data: "id=90$parent=john",                        //you can insert url argumnets here to pass to api.php
                                                  //for example "id=5&parent=6"
                 dataType: 'json',                //data format
                 success: function(data)          //on recieve of reply
